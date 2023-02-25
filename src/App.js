3a3b2a96745a481e93c9a7b1b45b5f859666a775
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2023-02-25 07:16:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(`${API_URL}`);
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div>
+    <h1>
+      User name is {data.name}
+    </h1>
+  </div>;
 }
 
 export default App;
